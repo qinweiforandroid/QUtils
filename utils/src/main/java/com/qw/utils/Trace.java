1f46ae4268aa12b3b8158ac734d42faf30140928
qinwei <qinwei_it@163.com>
2019-11-07 18:09:56 +0800

    日志工具添加接口可通过inject方法注入自己的实现类

--- utils/src/main/java/com/qw/utils/Trace.java
+++ utils/src/main/java/com/qw/utils/Trace.java
@@ -4,8 +4,8 @@ import android.util.Log;
 
 public class Trace {
     private static String TAG = "qinwei";
-
-    public static boolean model = true;
+    private static boolean debug = true;
+    private static ILog log;
 
     public static void v(String msg) {
         v(TAG, msg);
@@ -27,6 +27,22 @@ public class Trace {
         e(TAG, msg);
     }
 
+    public static void inject(ILog log) {
+        Trace.log = log;
+    }
+
+    public interface ILog {
+        void v(String tag, String msg);
+
+        void d(String tag, String msg);
+
+        void i(String tag, String msg);
+
+        void w(String tag, String msg);
+
+        void e(String tag, String msg);
+    }
+
 
     public static String getTraceInfo() {
         StringBuffer sb = new StringBuffer();
@@ -39,36 +55,60 @@ public class Trace {
     }
 
     public static void v(String tag, String msg) {
-        if (model) {
+        if (debug) {
             Log.v(tag, msg + "");
+        } else {
+            log.v(tag, msg);
         }
     }
 
     public static void d(String tag, String msg) {
-        if (model) {
+        if (debug) {
             Log.d(tag, msg + "");
+        } else {
+            log.d(tag, msg + "");
         }
     }
 
     public static void i(String tag, String msg) {
-        if (model) {
+        if (debug) {
             Log.i(tag, msg + "");
+        } else {
+            log.i(tag, msg + "");
         }
     }
 
     public static void w(String tag, String msg) {
-        if (model) {
+        if (debug) {
             Log.w(tag, msg + "");
+        } else {
+            log.w(tag, msg + "");
         }
     }
 
     public static void e(String tag, String msg) {
-        if (model) {
+        if (debug) {
             Log.e(tag, msg + "");
+        } else {
+            log.e(tag, msg + "");
         }
     }
 
+    /**
+     * 设置全局tag
+     *
+     * @param tag
+     */
     public static void setTag(String tag) {
         Trace.TAG = tag;
+    }
+
+    /**
+     * 设置开发模式
+     *
+     * @param debug
+     */
+    public static void setDebug(boolean debug) {
+        Trace.debug = debug;
     }
 }
