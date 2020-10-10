2f4c566f27eb470b354f977806edfeca2b4d1cc9
qinwei <qinwei_it@163.com>
2020-10-10 10:16:00 +0800

    fix 方法名字错误

--- utils/src/main/java/com/qw/utils/DensityUtil.java
+++ utils/src/main/java/com/qw/utils/DensityUtil.java
@@ -1,6 +1,7 @@
 package com.qw.utils;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Point;
 import android.util.DisplayMetrics;
 import android.view.Display;
@@ -36,5 +37,17 @@ public class DensityUtil {
     public static int getDPI(Context context) {
         DisplayMetrics dm = context.getResources().getDisplayMetrics();
         return dm.densityDpi;
+    }
+
+    /**
+     * 判断当前设备是手机还是平板，代码来自 Google I/O App for Android
+     *
+     * @param context
+     * @return 平板返回 True，手机返回 False
+     */
+    public static boolean isPadDevice(Context context) {
+        return (context.getResources().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK)
+                >= Configuration.SCREENLAYOUT_SIZE_LARGE;
     }
 }
