d47a01f23ef1fca8d322a56a88e038d2600bb9a9
qinwei <qinwei_it@163.com>
2022-08-08 08:29:26 +0800

    ResUtil

---
+++ utils/src/main/java/com/qw/utils/ResUtil.java
@@ -0,0 +1,20 @@
+package com.qw.utils;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.TypedValue;
+
+/**
+ * Created by qinwei on 2022/8/8 08:26
+ * email: qinwei_it@163.com
+ */
+public class ResUtil {
+    private final static TypedValue typedValue = new TypedValue();
+
+    public static int getColorOfAttr(Context context, int attr) {
+        if (context.getTheme().resolveAttribute(attr, typedValue, true)) {
+            return typedValue.data;
+        }
+        return Color.BLACK;
+    }
+}
